98e7b6d3b8ccf0a116ee30f1a434e8f93823c2ed
obrianmoreira <brian.agnilo@gmail.com>
2023-02-12 00:49:08 +0000

    responsive menu

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -3,8 +3,8 @@ import Head from 'next/head';
 import React, {useState} from 'react';
 import Link from 'next/link';
 import logo from '../Navbar/images/logo.png';
-/*import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
-import Layout from '../layout';*/
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+/* import Layout from '../layout';*/
 
 const Navbar = () => {
     return (
@@ -61,12 +61,27 @@ const MyNavbarLink = (props) => {
 
     return (
         <div>
-            <ul className='hidden sm:flex space-x-6 text-[19px]'>
+            <ul className='hidden lg:visible sm:flex space-x-6 text-[19px]'>
                 <li className='pt-2 hover:rounded hover:border-solid hover:text-white hover:bg-blue-900 px-[10px]'><Link href="/">Principal</Link></li>
                 <li className='pt-2 hover:rounded hover:border-solid hover:text-white hover:bg-blue-900 px-[10px]'><Link href='/'>Serviços</Link></li>
                 <li className='pt-2 hover:rounded hover:border-solid hover:text-white hover:bg-blue-900 px-[10px]' ><Link href="/" onMouseOver={MouseHover} onMouseOut={MouseOut}>Benefícios</Link></li>
                 <li className='bg-blue-900 hover:bg-blue-700 rounded text-white border-solid px-4 py-2' ><a href="https://wa.me/+351968943125">Aula Free</a></li>
             </ul>
+            <div onClick={handleNav} className='block sm:hidden z-10'>
+                {nav ?  <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+            </div>
+            <div className={
+                nav
+                    ? 'sm:hidden absolute top-20 mt-3 left-0 right-0 bottom-0 flex justify-left items-left pl-8 w-full h-fit bg-blue-900 text-left ease-in duration-300'
+                    : 'sm:hidden absolute top-20 mt-3 right-[-100%] right-0 bottom-0 flex justify-left items-left pl-8 w-full h-fit bg-blue-700 text-center ease-in duration-300'
+                }>
+                <ul className='text-white pb-2'>
+                    <li className='p-4 text-[27px]'><Link href="/">Home</Link></li>
+                    <li className='p-4 text-[27px]'><Link href="/">Services</Link></li>
+                    <li className='p-4 text-[27px]'><Link href="/">Benefits</Link></li>
+                    <li className='p-4 text-[27px]'><Link href="/">Free Hour</Link></li>
+                </ul>
+            </div>
             { openNav ? (  
                                     
                 <div className='text-[16px] text-center font-light'>
